6760c82a0825d07833a78c1cbab07baeee8cb6c7
BiancaGemarR <gr.bianca24@gmail.com>
2025-05-19 12:12:44 +0200

    sincronización

---
+++ modulo 0/agregar/sincronizacion.py
@@ -0,0 +1,23 @@
+#uso de lock para evitar condiciones de carreras
+import threading
+
+# Variable compartida
+saldo = 0
+lock = threading.Lock()  # Crear un Lock
+
+def depositar(dinero):
+    global saldo
+    for _ in range(100000):
+        with lock:  # Bloquear el acceso para evitar condiciones de carrera
+            saldo += dinero
+
+hilos = []
+for _ in range(2):
+    hilo = threading.Thread(target=depositar, args=(1,))
+    hilos.append(hilo)
+    hilo.start()
+
+for hilo in hilos:
+    hilo.join()
+
+print(f"Saldo final: {saldo}") 
