7bc586d7cfc9a432c47a7aeba98147827393476d
BiancaGemarR <gr.bianca24@gmail.com>
2025-05-20 13:04:09 +0200

    concurrencia y asincronías

---
+++ modulo 0/agregar/clase 57 asincio.py
@@ -0,0 +1,14 @@
+import asyncio
+
+async def process_data(data):
+    print(f'Procesando {data}...')
+    #simular una operación 
+    await asyncio.sleep(10)
+    print(f'{data} procesado.')
+    return data * 2
+async def main():
+    print('Inicio de procesamiento')
+    result = await process_data('archivo.txt')
+    print(f'Resultado: {result}')
+
+asyncio.run(main())
